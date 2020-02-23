c16917989d9d4a0619b89d550ffc5f68b8d9c952
Taylor Chen <s68175@gmail.com>
2020-02-23 15:58:34 +0800

    Add code

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+"""
+
+def toLowerCase(str):
+	return str.lower()
+
+print(toLowerCase("Hello"))
+print(toLowerCase("here"))
+print(toLowerCase("LOVELY"))
